10c6b9bb13a46467c729d9eb40ac65093cec26c3
mjin <myeongjin1220@gmail.com>
2017-12-07 10:09:26 +0900

    update all

--- public/js/charge_heart.js
+++ public/js/charge_heart.js
@@ -1,9 +1,51 @@
-function changeSelect(interest){
-    var div = interest.parentNode;
-    var a=0;
-    
-    for(var i = 0 ; i < div.getElementsByTagName('input').length ; i++)
-        div.getElementsByTagName('input')[i].setAttribute("id", "chooseHeart");
-    interest.setAttribute("id", "selected_heart");
+function changeSelect(interest) {
+  var div = interest.parentNode;
+  var a = 0;
 
+  for (var i = 0; i < div.getElementsByTagName('input').length; i++)
+    div.getElementsByTagName('input')[i].setAttribute("id", "chooseHeart");
+  interest.setAttribute("id", "selected_heart");
+}
+
+function getHeartAmount() {
+  var httpRequest;
+  if (window.XMLHttpRequest) { // 모질라, 사파리등 그외 브라우저, ...
+    httpRequest = new XMLHttpRequest();
+  } else if (window.ActiveXObject) { // IE 8 이상
+    httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  httpRequest.onreadystatechange = function () {
+    if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+      var res_json = JSON.parse(httpRequest.responseText);
+      document.getElementById('heart_amount').childNodes[0].nodeValue = res_json.data[0].heartNum;
+    }
+  };
+  httpRequest.open('GET', location.origin + '/api/heart', true);
+  httpRequest.setRequestHeader("Content-type", "application/json");
+  httpRequest.send();
+}
+
+function buyHeart(){
+  if(document.getElementById('selected_heart') == null){
+    alert('원하시는 하트의 개수를 선택해주세요.');
+    return;
+  }
+  var body = new Object;
+  body.changeNum = document.getElementById('selected_heart').parentNode.getElementsByClassName('amount')[0].getAttribute('id')
+
+  var httpRequest;
+  if (window.XMLHttpRequest) { // 모질라, 사파리등 그외 브라우저, ...
+    httpRequest = new XMLHttpRequest();
+  } else if (window.ActiveXObject) { // IE 8 이상
+    httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  httpRequest.onreadystatechange = function () {
+    if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+      alert('결제가 완료되었습니다.');
+      location.href = '/user/heart';
+    }
+  };
+  httpRequest.open('PUT', location.origin + '/api/heart', true);
+  httpRequest.setRequestHeader("Content-type", "application/json");
+  httpRequest.send(JSON.stringify(body));
 }
--- routes/api/heart/index.js
+++ routes/api/heart/index.js
@@ -3,13 +3,15 @@ var router = express.Router();
 
 var heartCtrler = require('../../../controllers/heart.ctrl');
 
+var authMiddleware = require('../../../middlewares/auth');
+
 /*
 	GET
 
 	Load heart.
 */
-router.get('/', function(req, res, next) {
-  var email = req.query.email;
+router.get('/', authMiddleware, function(req, res, next) {
+  var email = req.decoded.data.email;
 
   heartCtrler.loadUserHeart(email, function(error, resultObject){
     res.json(resultObject);
@@ -21,8 +23,8 @@ router.get('/', function(req, res, next) {
 
 	Update heart.
 */
-router.put('/', function(req, res, next) {
-  var email = req.body.email;
+router.put('/', authMiddleware, function(req, res, next) {
+  var email = req.decoded.data.email;
   var changeNum = req.body.changeNum;
 
   heartCtrler.changeHeart(email, changeNum, function(error, resultObject){
